1503176d8e941ac03ed7ab7b291066a357aae899
Vinicius Figueiredo <viniciusfs@gmail.com>
2010-08-13 17:52:59 -0300

    All dot files renamed to start with underline. Created a simple bash script to install files on $HOME directory.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+# Creates links on $HOME to all files starting with _ on current directory.
+# Underscores are replaced by dots on link file name. If a file already
+# exists it will be copied to $BACKUP_DIR before link creation.
+#
+
+BACKUP_DIR="${PWD}/$(hostname)-backup-$(date +%Y%m%d)"
+
+for file in _*; do
+	source="${PWD}/$file"
+	target="${HOME}/${file/_/.}"
+
+	if [ -e "${target}" ]; then
+		echo "${target} already exists, making backup..."
+		if [ ! -d ${BACKUP_DIR} ]; then
+			mkdir ${BACKUP_DIR}
+		fi
+		cp -Hr ${target} ${BACKUP_DIR}/${file/_/.}
+	fi
+	
+	ln -sf ${source} ${target}
+done
